94b9d70d460cb5d9e726ecc473dd65363d42beee
afernandezro7 <a.fernandez.ro7@gmail.com>
2021-04-26 09:59:35 -0500

    adding edit to video controller

--- src/AppBundle/Controller/VideoController.php
+++ src/AppBundle/Controller/VideoController.php
@@ -21,7 +21,7 @@ class VideoController extends Controller
         $data = array(
             'status'=> 'error',
             'code'=> 400,   
-            'msg'=>'Image not Uploaded'    
+            'msg'=>'Video not created'    
         );
 
         if(!$validateToken){
@@ -34,15 +34,14 @@ class VideoController extends Controller
         if(is_array($json)){
 
             $user= $helpers->currentUser($token);
-            $em = $this->getDoctrine()->getManager();
-            $video_repo= $em->getRepository('BackendBundle:Video');
+
 
             $createdAt = new \DateTime('now');
             $title=isset($json['title']) ? $json['title'] : null;
             $description= isset($json['description']) ? $json['description'] : null;
             $status= isset($json['status']) ? $json['status'] : null;
 
-            if($title != null || $description != null || $status != null){
+            if($user != null || $title != null || $description != null || $status != null){
                 $video= new Video();
                 $video->setCreatedAt($createdAt);
                 $video->setUpdatedAt($createdAt);
@@ -51,11 +50,97 @@ class VideoController extends Controller
                 $video->setStatus($status);
                 $video->SetUser($user);
 
-                var_dump($video);
-                die();
-            }
-        
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($video);
+                $em->flush();
 
+                $data = array(
+                    'status'=> 'success',
+                    'code'=> 200,   
+                    'msg'=>'Video created successfully',
+                    'video'=>$video 
+                );
+            }
+        }
+
+        return $helpers->toJson($data)->setStatusCode($data['code']);
+
+    }
+
+    public function editAction(Request $request)
+    {
+        
+        $helpers = $this->get("app.helpers");
+        $token = $request->headers->get('x-token',null);
+        $validateToken = $helpers->authCheck($token);
+        
+        $data = array(
+            'status'=> 'error',
+            'code'=> 400,   
+            'msg'=>'Video not created'    
+        );
+        
+        if(!$validateToken){
+            $data['msg']='Invalid Authorization';
+            return $helpers->toJson($data)->setStatusCode($data['code']);
+        }
+        
+        
+        $json= json_decode($request->getContent(),true);
+        
+        if(is_array($json)){
+            
+            $user= $helpers->currentUser($token);
+            
+            
+            $id=isset($json['id']) ? $json['id'] : null;
+            $updatedAt = new \DateTime('now');
+            $title=isset($json['title']) ? $json['title'] : null;
+            $description= isset($json['description']) ? $json['description'] : null;
+            $status= isset($json['status']) ? $json['status'] : null;
+
+            //Buscar id del usuario activo en db
+            $em = $this->getDoctrine()->getManager();
+            $video_repo= $em->getRepository('BackendBundle:Video');
+            $video= $video_repo->findOneBy(array('id'=>$id));
+            
+            if(is_object($video)){
+                
+                if($user == null || $user->getId() != $video->getUser()->getId()){
+                    $data['msg']='Invalid Authorization, this asset is not yours';
+                    return $helpers->toJson($data)->setStatusCode($data['code']);
+                }
+
+                $video->setUpdatedAt($updatedAt);
+               
+                if($title){
+                    $video->setTitle($title);
+                }
+                if($description){
+                    $video->setDescription($description);
+                }
+                if($status){
+                    $video->setStatus($status);
+                }
+
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($video);
+                $em->flush();
+
+                $data = array(
+                    'status'=> 'success',
+                    'code'=> 200,   
+                    'msg'=>'Video updated successfully',
+                    'video'=>$video 
+                );
+            }else{
+                $data = array(
+                    'status'=> 'error',
+                    'code'=> 404,   
+                    'msg'=>'Video not found'    
+                );
+            }
         }
 
         return $helpers->toJson($data)->setStatusCode($data['code']);
